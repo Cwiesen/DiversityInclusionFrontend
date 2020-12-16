0294843dec114027fb0098839803cd52bac12758
Jesse Lavendt <jlavendt@C02CMRB3ML7H.fios-router.home>
2020-12-16 14:16:19 -0500

    added demographics page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonsComponent } from './persons/persons.component';
 import {PreloadService} from './preload.service';
+import {DemographicsComponent} from './demographics/demographics.component';
 
 const routes: Routes = [
   { path: 'persons', component: PersonsComponent, 
   resolve: { people: PreloadService}
-}
+},
+  { path: 'demographics', component: DemographicsComponent}
 ];
 
 @NgModule({
